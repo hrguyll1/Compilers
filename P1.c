4fbe0473de7917bbd01fcbc0c0108cf48e2ac798
root <hrg0011@utulsa.edu>
2017-08-29 14:22:25 -0500

    Updated whitespace machine

--- P1.c
+++ P1.c
@@ -5,59 +5,150 @@
 #define SIZE 72
 int f = 0;
 int p = 0;
-int line = 0;
+int line = 1;
+FILE *file = NULL;
+FILE *outfile = NULL;
+int clear(char* buffer){
+	int j = 0;
+	for(j = 0; j < 72; j++){
+		buffer[j] = '\0';
+	}
+}
+
+/*
+	Real Second!
+*/
+/*
+	Identifer:
+	Up to 10 character
+	Starts character
+	Can have numbers after
+*/
+int identifier(char* buffer){
+	int j = 0;
+	if(isalpha(buffer[0])){
+		//check if each of the others are alpha or digit
+		j = 1;
+		while(1){
+			//got an identifier token
+			if(buffer[j] == ' '){
+				int k = 0;
+				for(k = 0; k < j; k++){
+					printf("%c", buffer[k]);
+				}
+				printf("\n");
+				/*
+					Got a token. Update the buffer to have something other than the token
+				*/
+				return 1;
+			}
+			if(isalpha(buffer[j]) || isdigit(buffer[j])){
+				if( j >= 10){
+					printf("identifier too long\n");
+				}
+				j = j + 1;
+			}else{
+				printf("not correct ID format with buffer = %s\n", buffer);
+				return -1;
+			}
+		}
+	}else{
+		//not an identifier
+		printf("not correct ID format\n");
+		return -1;
+	}
+}
+int real(char* buffer){
+
+
+
+	return 0;
+}
+/*
+	Long Real first
+*/
 
 int relop(char* buffer){
 
+
+
+
+	return 0;
+}
+/*
+	If reach /0 then read next buffer
+*/
+int readline(char* buffer){
+	int i = 0;
+	char ch;
+	int newline = 0;
+	int j = 0;
+	while((ch = fgetc(file)) != '\n'){
+		if(ch == EOF){
+			return -1;
+		}
+		//While it doesn't equal newline, put it into buffer
+		buffer[i] = ch;
+		//do something with buffer
+		i++;
+	}
+	buffer[i] = '\n';
 	return 0;
 }
 int whitespace(char* buffer){
 	int i = 0;
-	while(i < SIZE){
+	while(1){
 		//Look for something other than whitespace
-		if(buffer[i] != '\r' && buffer[i] != '\n' && buffer[i] != '\t' && buffer[i] != ' '){
+		if(buffer[i] == '\n'){
+			clear(buffer);
+			int status = readline(buffer);
+			if(status == -1){
+				printf("End of file.\n");
+				break;
+			}
+			i = 0;
+		}else if(buffer[i] == '\t' || buffer[i] == ' '){
 			//found something other than a space
 			//update index f
+			/*
+				Trim the buffer by shifting it
+			*/
+			i = i + 1;
+		}else{
 			f = i;
-			printf("%c", buffer[f]);
+			printf("char[%i] = %c\n", i, buffer[f]);
 			return 1;
 
 		}
-		i = i + 1;
 	}
 
-	return 0;
-}
-
-int readline(char* buffer){
-	FILE *file = fopen("sourcefile.txt", "r");
-	FILE *outfile = fopen("outfile.txt", "w");
-	int i = 0;
-	char ch;
-	int newline = 0;
-	while((ch = fgetc(file)) != EOF){
-		//While it doesn't equal newline, put it into buffer
-		buffer[i] = ch;
-		if(ch == '\n'){
-			line = line + 1;
-			//do something with buffer
-			printf("%d.    %s", line, buffer);
-			whitespace(buffer);
-			//clear buffer
-			int j = 0;
-			for(j = 0; j < SIZE; j++){
-				buffer[j] = '\0';
-			}
-			i = -1;
-		}
-		i++;
-	}
 	return 0;
 }
 
 int main(){
+	file = fopen("sourcefile.txt", "r");
+	outfile = fopen("outfile.txt", "w");
+
 	char buffer[SIZE];
-	readline(buffer);
+	int i = 0;
+	int j = 0;
+	int status = 0;
+	status = readline(buffer);
+	if(status == -1){
+		printf("End of file.\n");
+	}else{
+		whitespace(buffer);
+		identifier(buffer);
+	}
+//	while(status != -1){
+//		//printf("%i.    %s\n", line, buffer);
+//		whitespace(buffer);
+//		clear(buffer);
+//		line = line + 1;
+//		i++;
+//		status = readline(buffer);
+
+//	}
 /*
 	FILE *file;
 	FILE *outfile = fopen("outfile.txt", "w");
